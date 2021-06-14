695d016492848f46e49adfc19d58399075764393
sang <minhsang2001.sms@gmail.com>
2021-06-14 09:06:04 +0700

    Init Database

---
+++ QLFILM/Migrations/202106140149413_AddDataIntoCategories.cs
@@ -0,0 +1,33 @@
+﻿namespace QLFILM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataIntoCategories : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C001', N'Romance');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C002', N'Adventure');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C003', N'Mystery');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C004', N'Drama');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C005', N'Horror');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C006', N'Sport');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C007', N'Comedy');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C008', N'War');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C009', N'History');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C010', N'Crime');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C011', N'Fantasy');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C012', N'Sci-Fi');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C013', N'Musical');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C014', N'Music');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C015', N'Family');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C016', N'Thriller');");
+            Sql("INSERT INTO Categories (CID, CategoryName) VALUES (N'C017', N'Action');");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ QLFILM/Migrations/202106140200506_AddDataIntoDirectorFilms.cs
@@ -0,0 +1,120 @@
+﻿namespace QLFILM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataIntoDirectorFilms : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F001', N'D058');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F002', N'D058');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F003', N'D058');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F004', N'D051');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F005', N'D055');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F006', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F007', N'D010');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F008', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F009', N'D042');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F010', N'D024');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F011', N'D006');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F012', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F013', N'D002');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F014', N'D015');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F015', N'D019');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F016', N'D042');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F017', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F018', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F019', N'D035');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F020', N'D014');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F021', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F022', N'D014');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F023', N'D008');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F023', N'D003');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F024', N'D030');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F025', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F026', N'D032');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F027', N'D021');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F028', N'D045');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F029', N'D050');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F030', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F031', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F032', N'D035');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F033', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F034', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F035', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F036', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F037', N'D045');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F038', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F039', N'D043');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F040', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F041', N'D033');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F042', N'D037');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F043', N'D020');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F044', N'D027');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F045', N'D006');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F046', N'D034');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F047', N'D024');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F048', N'D016');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F049', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F050', N'D015');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F051', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F052', N'D030');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F053', N'D047');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F054', N'D030');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F055', N'D029');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F056', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F057', N'D031');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F058', N'D054');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F059', N'D030');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F060', N'D051');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F061', N'D011');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F062', N'D050');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F063', N'D059');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F064', N'D041');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F065', N'D012');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F066', N'D044');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F066', N'D036');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F067', N'D022');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F068', N'D056');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F068', N'D021');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F069', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F070', N'D053');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F071', N'D038');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F071', N'D017');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F072', N'D004');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F073', N'D021');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F074', N'D039');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F075', N'D059');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F076', N'D007');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F077', N'D057');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F078', N'D046');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F079', N'D039');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F080', N'D052');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F081', N'D045');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F082', N'D019');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F083', N'D009');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F084', N'D019');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F085', N'D023');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F086', N'D045');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F087', N'D001');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F088', N'D025');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F089', N'D005');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F090', N'D031');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F091', N'D026');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F092', N'D049');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F093', N'D040');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F094', N'D013');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F095', N'D006');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F096', N'D028');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F097', N'D020');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F098', N'D028');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F099', N'D048');");
+            Sql("INSERT INTO DirectorFilms (Film_FID, Director_DID) VALUES (N'F100', N'D018');");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
